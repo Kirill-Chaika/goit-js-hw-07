00e0eef3b3c8eea935183e2828409bae0964aa03
Kirill-Chaika <115115398+Kirill-Chaika@users.noreply.github.com>
2023-06-28 08:25:45 +0300

    assaa

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,94 +1,50 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 
-// const galleryContainer = document.querySelector('.gallery');
-// const galleryCardsSet = createOfGallery(galleryItems);
-
-// function createOfGallery(galleryItems) {
-//   return galleryItems
-//     .map(({ original, preview, description }) => {
-//       return `<div class="gallery__item">
-//       <a class="gallery__link" ${original} ">
-//         <img
-//           class="gallery__image"
-//           src="${preview}"
-//           data-source="${original}"
-//           alt="${description}"
-//         />
-//       </a>
-//     </div>`;
-//     })
-//     .join('');
-// }
-// galleryContainer.insertAdjacentHTML('beforeend', galleryCardsSet);
-// galleryContainer.addEventListener('click', selectGalleryEl);
-
-// function selectGalleryEl(event) {
-//   const galleryImage = event.target.classList.contains('gallery__image');
-//    if (!galleryImage){
-//        return;
-//    }
-//   const instance = basicLightbox.create(
-//     `<img src="${event.target.dataset.source} " width="800" height="600">`,
-//     {
-//       onShow: () => {
-//         window.addEventListener('keydown', onKeydownEsc);
-//       },
-//       onClose: () => {
-//         window.removeEventListener('keydown', onKeydownEsc);
-//       },
-//     },
-//   );
-
-//   const onKeydownEsc = event => {
-//     console.log(event.code);
-//     if (event.code === 'Escape') {
-//       instance.close();
-//     }
-//   };
-
-//   instance.show();
-// }
-
-const galleryContainer = document.querySelector('.gallery');
+const galleryContainer = document.querySelector(".gallery");
 const galleryCardsSet = createOfGallery(galleryItems);
 
 function createOfGallery(galleryItems) {
   return galleryItems
-  .map(({original, preview, description}) => {
-    return `<div class="gallery__item">
-         <a class="gallery__link" ${original} ">
-           <img
-             class="gallery__image"
-             src="${preview}"
-             data-source="${original}"
-             alt="${description}"
-           />
-         </a>
-       </div>`;
-  }).join('');
-} 
-galleryContainer.insertAdjacentHTML('beforeend', galleryCardsSet);
-galleryContainer.addEventListener('click', selectGalerryEl);
+    .map(({ original, preview, description }) => {
+      return `<div class="gallery__item">
+      <a class="gallery__link" ${original} ">
+        <img
+          class="gallery__image"
+          src="${preview}"
+          data-source="${original}"
+          alt="${description}"
+        />
+      </a>
+    </div>`;
+    })
+    .join("");
+}
+galleryContainer.insertAdjacentHTML("beforeend", galleryCardsSet);
+galleryContainer.addEventListener("click", selectGalleryEl);
 
-function selectGalerryEl(event) {
-  const galleryImage = event.target.classLict.contains('gallery__image');
-  if (!galleryImage){
+function selectGalleryEl(event) {
+  const galleryImage = event.target.classList.contains("gallery__image");
+  if (!galleryImage) {
     return;
   }
-  const instance = basicLightbox.create(`<img src="${event.target.dataset.source} " width="800" height="600">`, {
-    onShow: () => {
-      window.addEventListener('keydown', onKeydownEsc);
-    },onClose: () => {
-      window.removeEventListener('keydown', onKeydownEsc);
-    },
-  },
-  ); 
+  const instance = basicLightbox.create(
+    `<img src="${event.target.dataset.source} " width="800" height="600">`,
+    {
+      onShow: () => {
+        window.addEventListener("keydown", onKeydownEsc);
+      },
+      onClose: () => {
+        window.removeEventListener("keydown", onKeydownEsc);
+      },
+    }
+  );
 
-  const onKeydownEsc = event => {
+  const onKeydownEsc = (event) => {
     console.log(event.code);
-    if(event.code === 'Escape'){
+    if (event.code === "Escape") {
       instance.close();
     }
   };
-  instance.show()
+
+  instance.show();
 }
